fac1093a03a5728ff558ad68ef42444993a8842e
therealveggie <gv.veggie.321@gmail.com>
2018-02-01 20:41:49 -0500

    Instead of making 2 rows of columns I need to make 3 columns of 2 rows

--- js/team.js
+++ js/team.js
@@ -1,4 +1,92 @@
+var hidden = [true,true, true];
 $(document).ready(function() {
 	var h = Math.max($('#dev').height(), $('#stc').height(), $('#stc').height());
-	$('.column').height(h)
+	$('.column').height(h);
+	$('#hidden_dev').hide();
+	$('#hidden_sme').hide();
+	//$('#hidden_stc').hide();
+
+	$('#dev_drop').click(
+	function(){
+		var visible=false;
+		var src = ($(this).attr('src') === 'images/DownArrow.png')
+            ? visible=true
+            : visible=false;
+        if(visible)
+        {
+        	$(this).attr('src', 'images/UpArrow.png');
+        	$('#hidden_dev').show();	
+        }
+        else
+        {
+        	$(this).attr('src', 'images/DownArrow.png');
+        	$('#hidden_dev').hide();	
+        }
+		}
+	);
+
+	$('#hidden_dev').mouseleave(
+		function()
+		{
+			$('#dev_drop').attr('src', 'images/DownArrow.png');
+        	$('#hidden_dev').hide();	
+		}
+	);
+
+	$('#stc_drop').click(
+	function(){
+		var visible=false;
+		var src = ($(this).attr('src') === 'images/DownArrow.png')
+            ? visible=true
+            : visible=false;
+        if(visible)
+        {
+        	$(this).attr('src', 'images/UpArrow.png');
+        	$('#hidden_stc').show();	
+        }
+        else
+        {
+        	$(this).attr('src', 'images/DownArrow.png');
+        	$('#hidden_stc').hide();	
+        }
+		}
+	);
+
+	$('#hidden_stc').mouseleave(
+		function()
+		{
+			$('#stc_drop').attr('src', 'images/DownArrow.png');
+        	$('#hidden_stc').hide();	
+		}
+	);
+
+
+	$('#sme_drop').click(
+	function(){
+		var visible=false;
+		var src = ($(this).attr('src') === 'images/DownArrow.png')
+            ? visible=true
+            : visible=false;
+        if(visible)
+        {
+        	$(this).attr('src', 'images/UpArrow.png');
+        	$('#hidden_sme').show();	
+        }
+        else
+        {
+        	$(this).attr('src', 'images/DownArrow.png');
+        	$('#hidden_sme').hide();	
+        }
+		}
+	);
+
+	$('#hidden_dev').mouseleave(
+		function()
+		{
+			$('#sme_drop').attr('src', 'images/DownArrow.png');
+        	$('#hidden_sme').hide();	
+		}
+	);
 });
+
+
