97dd19f5704acc1555424e6f09c79ae2a15ad769
therealveggie <gv.veggie.321@gmail.com>
2018-02-05 09:58:59 -0500

    got the alignment of dropdown in Team working, need to align the arrow to the bottom

--- js/team.js
+++ js/team.js
@@ -1,10 +1,11 @@
 var hidden = [true,true, true];
 $(document).ready(function() {
-	var h = Math.max($('#dev').height(), $('#stc').height(), $('#stc').height());
+	var h = Math.max($('#dev').height() - $('#hidden_dev').height(), $('#stc').height() - $('#hidden_sme').height(), $('#stc').height() - $('#hidden_stc').height());
+	
 	$('.column').height(h);
 	$('#hidden_dev').hide();
 	$('#hidden_sme').hide();
-	//$('#hidden_stc').hide();
+	$('#hidden_stc').hide();
 
 	$('#dev_drop').click(
 	function(){
@@ -15,11 +16,13 @@ $(document).ready(function() {
         if(visible)
         {
         	$(this).attr('src', 'images/UpArrow.png');
+			$("#dev").height(h + $('#hidden_dev').height());
         	$('#hidden_dev').show();	
         }
         else
         {
         	$(this).attr('src', 'images/DownArrow.png');
+			$("#dev").height(h);
         	$('#hidden_dev').hide();	
         }
 		}
@@ -29,7 +32,8 @@ $(document).ready(function() {
 		function()
 		{
 			$('#dev_drop').attr('src', 'images/DownArrow.png');
-        	$('#hidden_dev').hide();	
+        	$("#dev").height(h);
+			$('#hidden_dev').hide();	
 		}
 	);
 
@@ -42,11 +46,13 @@ $(document).ready(function() {
         if(visible)
         {
         	$(this).attr('src', 'images/UpArrow.png');
+			$("#stc").height(h + $('#hidden_stc').height());
         	$('#hidden_stc').show();	
         }
         else
         {
         	$(this).attr('src', 'images/DownArrow.png');
+			$("#stc").height(h);
         	$('#hidden_stc').hide();	
         }
 		}
@@ -56,6 +62,7 @@ $(document).ready(function() {
 		function()
 		{
 			$('#stc_drop').attr('src', 'images/DownArrow.png');
+			$("#stc").height(h);
         	$('#hidden_stc').hide();	
 		}
 	);
@@ -70,20 +77,23 @@ $(document).ready(function() {
         if(visible)
         {
         	$(this).attr('src', 'images/UpArrow.png');
+			$("#sme").height(h + $('#hidden_sme').height());
         	$('#hidden_sme').show();	
         }
         else
         {
         	$(this).attr('src', 'images/DownArrow.png');
+			$("#sme").height(h);
         	$('#hidden_sme').hide();	
         }
 		}
 	);
 
-	$('#hidden_dev').mouseleave(
+	$('#hidden_sme').mouseleave(
 		function()
 		{
 			$('#sme_drop').attr('src', 'images/DownArrow.png');
+			$("#sme").height(h);
         	$('#hidden_sme').hide();	
 		}
 	);
